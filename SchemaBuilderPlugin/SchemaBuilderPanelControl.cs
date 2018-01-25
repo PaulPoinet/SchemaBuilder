1f9f4694c450799382a85bcecf5ff268dda9499c
Luis Fraguada <fraguada@gmail.com>
2018-01-25 12:46:26 +0100

    Bumped Cef version to 53, moved Cef init code to plugin class. Fixes #1.

--- SchemaBuilderPlugin/SchemaBuilderPanelControl.cs
+++ SchemaBuilderPlugin/SchemaBuilderPanelControl.cs
@@ -4,66 +4,71 @@ using CefSharp;
 using CefSharp.WinForms;
 using System.Reflection;
 using System.IO;
+using System.Diagnostics;
 
 namespace SchemaBuilder
 {
-  [System.Runtime.InteropServices.Guid("A687BDD9-F74C-4BB2-88E0-E2AEC95A9FCE")]
-  public partial class SchemaBuilderPanelControl : UserControl
-  {
-    private ChromiumWebBrowser m_browser;
-
-    /// <summary>
-    /// Returns the ID of this panel.
-    /// </summary>
-    public static Guid PanelId
+    [System.Runtime.InteropServices.Guid("A687BDD9-F74C-4BB2-88E0-E2AEC95A9FCE")]
+    public partial class SchemaBuilderPanelControl : UserControl
     {
-      get
-      {
-        return typeof(SchemaBuilderPanelControl).GUID;
-      }
+        private ChromiumWebBrowser m_browser;
+
+        /// <summary>
+        /// Returns the ID of this panel.
+        /// </summary>
+        public static Guid PanelId
+        {
+            get
+            {
+                return typeof(SchemaBuilderPanelControl).GUID;
+            }
+        }
+
+        public SchemaBuilderPanelControl()
+        {
+            InitializeComponent();
+            InitializeBrowser();
+            SchemaBuilderPlugIn.Instance.UserControl = this;
+            this.Disposed += new EventHandler(OnDisposed);
+        }
+
+        private void InitializeBrowser()
+        {
+
+            if (Cef.IsInitialized)
+            {
+
+                m_browser = new ChromiumWebBrowser("http://localhost:9090/");
+
+                m_browser.BrowserSettings = new BrowserSettings
+                {
+                    FileAccessFromFileUrls = CefState.Enabled,
+                    UniversalAccessFromFileUrls = CefState.Enabled
+                };
+
+                this.Controls.Add(m_browser);
+                m_browser.Dock = DockStyle.Fill;
+
+                m_browser.RegisterAsyncJsObject("Interop", new Interop(m_browser));
+            }
+            else
+            {
+                Debug.WriteLine("For some reason, Cef didn't initialize", "SchemaBuilder");
+            }
+
+        }
+
+        /// <summary>
+        /// Occurs when the component is disposed by a call to the
+        /// System.ComponentModel.Component.Dispose() method.
+        /// </summary>
+        private void OnDisposed(object sender, EventArgs e)
+        {
+            m_browser.Dispose();
+            Cef.Shutdown();
+            SchemaBuilderPlugIn.Instance.UserControl = null;
+        }
+
+
     }
-
-    public SchemaBuilderPanelControl()
-    {
-      InitializeComponent();
-      InitializeBrowser();
-      SchemaBuilderPlugIn.Instance.UserControl = this;
-      this.Disposed += new EventHandler(OnDisposed);
-    }
-
-    private void InitializeBrowser()
-    {
-      Cef.EnableHighDPISupport();
-
-      string assemblyLocation = Assembly.GetExecutingAssembly().Location;
-      string assemblyPath = Path.GetDirectoryName(assemblyLocation);
-      string pathSubprocess = Path.Combine(assemblyPath, "CefSharp.BrowserSubprocess.exe");
-
-      var settings = new CefSettings();
-      settings.BrowserSubprocessPath = pathSubprocess;
-      settings.CefCommandLineArgs.Add("disable-gpu", "1");
-      Cef.Initialize(settings);
-
-      m_browser = new ChromiumWebBrowser("http://localhost:9090/");
-      Controls.Add(m_browser);
-      m_browser.Dock = DockStyle.Fill;
-      m_browser.Enabled = true;
-      m_browser.RegisterAsyncJsObject("Interop", new Interop(m_browser));
-      m_browser.Show();
-      
-    }
-
-    /// <summary>
-    /// Occurs when the component is disposed by a call to the
-    /// System.ComponentModel.Component.Dispose() method.
-    /// </summary>
-    private void OnDisposed(object sender, EventArgs e)
-    {
-      m_browser.Dispose();
-      Cef.Shutdown();
-      SchemaBuilderPlugIn.Instance.UserControl = null;
-    }
-    
-
-  }
 }
--- SchemaBuilderPlugin/SchemaBuilderPlugIn.cs
+++ SchemaBuilderPlugin/SchemaBuilderPlugIn.cs
@@ -1,48 +1,75 @@
-﻿namespace SchemaBuilder
+﻿using CefSharp;
+using System.IO;
+using System.Reflection;
+
+namespace SchemaBuilder
 {
-  ///<summary>
-  /// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
-  /// class. DO NOT create instances of this class yourself. It is the
-  /// responsibility of Rhino to create an instance of this class.</para>
-  /// <para>To complete plug-in information, please also see all PlugInDescription
-  /// attributes in AssemblyInfo.cs (you might need to click "Project" ->
-  /// "Show All Files" to see it in the "Solution Explorer" window).</para>
-  ///</summary>
-  public class SchemaBuilderPlugIn : Rhino.PlugIns.PlugIn
-  {
-    public SchemaBuilderPlugIn()
+    ///<summary>
+    /// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
+    /// class. DO NOT create instances of this class yourself. It is the
+    /// responsibility of Rhino to create an instance of this class.</para>
+    /// <para>To complete plug-in information, please also see all PlugInDescription
+    /// attributes in AssemblyInfo.cs (you might need to click "Project" ->
+    /// "Show All Files" to see it in the "Solution Explorer" window).</para>
+    ///</summary>
+    public class SchemaBuilderPlugIn : Rhino.PlugIns.PlugIn
     {
-      Instance = this;
-    }
+        public SchemaBuilderPlugIn()
+        {
+            Instance = this;
+        }
 
-    ///<summary>Gets the only instance of the SampleCsChromiumPlugIn plug-in.</summary>
-    public static SchemaBuilderPlugIn Instance
-    {
-      get;
-      private set;
-    }
+        ///<summary>Gets the only instance of the SampleCsChromiumPlugIn plug-in.</summary>
+        public static SchemaBuilderPlugIn Instance
+        {
+            get;
+            private set;
+        }
 
-    // You can override methods here to change the plug-in behavior on
-    // loading and shut down, add options pages to the Rhino _Option command
-    // and mantain plug-in wide options in a document.
+        // You can override methods here to change the plug-in behavior on
+        // loading and shut down, add options pages to the Rhino _Option command
+        // and mantain plug-in wide options in a document.
 
-    /// <summary>
-    /// Called when the plug-in is being loaded.
-    /// </summary>
-    protected override Rhino.PlugIns.LoadReturnCode OnLoad(ref string errorMessage)
-    {
-      var panel_type = typeof(SchemaBuilderPanelControl);
-      Rhino.UI.Panels.RegisterPanel(this, panel_type, "SchemaBuilder", SchemaBuilder.Properties.Resources.SchemaBuilder);
-      return Rhino.PlugIns.LoadReturnCode.Success;
-    }
+        /// <summary>
+        /// Called when the plug-in is being loaded.
+        /// </summary>
+        protected override Rhino.PlugIns.LoadReturnCode OnLoad(ref string errorMessage)
+        {
+            var panel_type = typeof(SchemaBuilderPanelControl);
+            Rhino.UI.Panels.RegisterPanel(this, panel_type, "SchemaBuilder", SchemaBuilder.Properties.Resources.SchemaBuilder);
+            return Rhino.PlugIns.LoadReturnCode.Success;
+        }
 
-    /// <summary>
-    /// Gets tabbed dockbar user control
-    /// </summary>
-    public SchemaBuilderPanelControl UserControl
-    {
-      get;
-      set;
+        void InitializeCef()
+        {
+            Cef.EnableHighDPISupport();
+
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            string assemblyPath = Path.GetDirectoryName(assemblyLocation);
+            string pathSubprocess = Path.Combine(assemblyPath, "CefSharp.BrowserSubprocess.exe");
+
+            CefSettings settings = new CefSettings
+            {
+                LogSeverity = LogSeverity.Verbose,
+                LogFile = "ceflog.txt",
+                BrowserSubprocessPath = pathSubprocess,
+            };
+
+            //Not needed in Rhino 6
+            settings.CefCommandLineArgs.Add("disable-gpu", "1");
+
+            // Initialize cef with the provided settings
+            if (!Cef.IsInitialized)
+                Cef.Initialize(settings);
+        }
+
+        /// <summary>
+        /// Gets tabbed dockbar user control
+        /// </summary>
+        public SchemaBuilderPanelControl UserControl
+        {
+            get;
+            set;
+        }
     }
-  }
 }
